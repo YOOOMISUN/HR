bdf32cd3016eaa676c61f09443e98e3150c482be
YOOOMISUN <sis3841@naver.com>
2022-09-14 18:25:09 +0900

    log color 추가

---
+++ hr/src/main/java/com/gd/hr/commons/Color.java
@@ -0,0 +1,8 @@
+package com.gd.hr.commons;
+
+public class Color {
+	public static final String RED = "\u001B[31m";	// 레드
+	public static final String BLUE = "\u001B[34m";	// 
+	public static final String BLACK = "\u001B[30m";	
+	public static final String CYAN = "\u001B[36m";	
+}
--- hr/src/main/java/com/gd/hr/controller/BoardController.java
+++ hr/src/main/java/com/gd/hr/controller/BoardController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.gd.hr.service.IBoardService;
 import com.gd.hr.vo.BoardForm;
@@ -43,6 +44,14 @@ public class BoardController {
 		return "redirect:/boardList";
 		}
 
+	@PostMapping("/removeBoard")
+	public String removeBoard(@RequestParam(value="boardNo") int boardNo) {
+		int remove = boardService.removeBoard(boardNo);
+		
+		
+		return null;
+		
+	}
 	
 
 
